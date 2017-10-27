539f94a4a05439a88addcd3347cac8cad8beb381
LinuxLover3000 <charlieyonkura@gmail.com>
2017-10-27 16:36:30 -0400

    Update metar.bat

--- metar.bat
+++ metar.bat
@@ -1,4 +1,5 @@
 @echo off
+:start
 echo VFSControl METAR Creator
 echo.
 echo So Far: METAR .... ......Z ..... ..SM ..... ../.. A.... RMK .
@@ -38,4 +39,92 @@ echo.
 set /p zulu="Enter: "
 if %zulu%==exit or exiT or exIt or exIT or eXit or eXiT or eXIt or eXIT or Exit or ExiT or ExIt or ExIT or EXit or EXiT or EXIT (
 exit
+)
+cls
+echo VFSControl METAR Creator
+echo.
+echo So Far: METAR %icao% %day%%zulu%Z *..*SM ..... ../.. A.... RMK .
+echo.
+echo Enter the visibility in statued miles between 1 and 20.
+echo ex. 15
+echo.
+set /p visi="Enter: "
+if %visi%==exit or exiT or exIt or exIT or eXit or eXiT or eXIt or eXIT or Exit or ExiT or ExIt or ExIT or EXit or EXiT or EXIT (
+exit
+)
+cls
+echo VFSControl METAR Creator
+echo.
+echo So Far: METAR %icao% %day%%zulu%Z %visi%SM *.....* ../.. A.... RMK .
+echo.
+echo Enter the direction and speed of the wind.
+echo ex. 190 degrees at 20 knots is 19020.
+echo.
+set /p wind="Enter: "
+if %wind%==exit or exiT or exIt or exIT or eXit or eXiT or eXIt or eXIT or Exit or ExiT or ExIt or ExIT or EXit or EXiT or EXIT (
+exit
+)
+cls
+echo VFSControl METAR Creator
+echo.
+echo So Far: METAR %icao% %day%%zulu%Z %visi%SM %wind% *..*/.. A.... RMK .
+echo.
+echo Enter the temperature in Celsius.
+echo.
+set /p temp="Enter: "
+if %temp%==exit or exiT or exIt or exIT or eXit or eXiT or eXIt or eXIT or Exit or ExiT or ExIt or ExIT or EXit or EXiT or EXIT (
+exit
+)
+cls
+echo VFSControl METAR Creator
+echo.
+echo So Far: METAR %icao% %day%%zulu%Z %visi%SM %wind% %temp%/*..* A.... RMK .
+echo.
+echo Enter the dewpoint in Celsius.
+echo.
+set /p dewp="Enter: "
+if %dewp%==exit or exiT or exIt or exIT or eXit or eXiT or eXIt or eXIT or Exit or ExiT or ExIt or ExIT or EXit or EXiT or EXIT (
+exit
+)
+cls
+echo VFSControl METAR Creator
+echo.
+echo So Far: METAR %icao% %day%%zulu%Z %visi%SM %wind% %temp%/%dewp% A*....* RMK .
+echo.
+echo Enter the altimiter setting.
+echo.
+set /p alti="Enter: "
+if %alti%==exit or exiT or exIt or exIT or eXit or eXiT or eXIt or eXIT or Exit or ExiT or ExIt or ExIT or EXit or EXiT or EXIT (
+exit
+)
+cls
+echo VFSControl METAR Creator
+echo.
+echo So Far: METAR %icao% %day%%zulu%Z %visi%SM %wind% %temp%/%dewp% A%alti% RMK .
+echo.
+echo Enter any remarks you may want to add.
+echo.
+set /p rmrk="Enter: "
+if %rmrk%==exit or exiT or exIt or exIT or eXit or eXiT or eXIt or eXIT or Exit or ExiT or ExIt or ExIT or EXit or EXiT or EXIT (
+exit
+)
+cls
+echo VFSControl METAR Creator
+echo.
+echo Your METAR: METAR %icao% %day%%zulu%Z %visi%SM %wind% %temp%/%dewp% A%alti% RMK %rmrk%
+echo.
+echo Save this METAR?
+echo 1) Yes
+echo 2) No
+echo.
+set /p save="Enter: "
+if %save%==exit or exiT or exIt or exIT or eXit or eXiT or eXIt or eXIT or Exit or ExiT or ExIt or ExIT or EXit or EXiT or EXIT (
+exit
+)
+if %save%==1 (
+echo METAR %icao% %day%%zulu%Z %visi%SM %wind% %temp%/%dewp% A%alti% RMK %rmrk%>METAR%icao%%day%%zulu%.txt
+goto start
+)
+if %save%==2 (
+goto start
 )
